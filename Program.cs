363cb44ca596ab2f76a3af08ff9745f4449626d9
amrit <amrit@AMRITSRAZERBLAD>
2023-05-04 09:13:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalQ1
+{
+    internal class Program
+    {
+        class MyStack
+        {
+            private List<int> list;
+
+            public MyStack()
+            {
+                list = new List<int>();
+            }
+
+            public void Push(int n)
+            {
+                list.Add(n);
+            }
+
+            public int Pop()
+            {
+                int lastItem = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                return lastItem;
+            }
+
+            public int Peek()
+            {
+                return list[list.Count - 1];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+            myStack.Push(1);
+            myStack.Push(2);
+
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Peek());
+        }
+    }
+}
